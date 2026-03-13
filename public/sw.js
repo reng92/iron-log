3303c0362df846d37159d1c7199405d79c370dcd
reng92 <renatogambardella@gmail.com>
2026-03-13 11:35:11 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// Questo basta per far credere al browser che siamo una PWA completa
+self.addEventListener('fetch', function(event) {
+  // Per ora non facciamo nulla di speciale, ma il browser è contento
+});
